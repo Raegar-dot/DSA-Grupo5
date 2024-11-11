b79a8946fc9fd6b43893fe405b74efaed90e3c2f
da.rojasc123 <da.rojasc123@uniandes.edu.co>
2024-11-11 12:16:08 -0500

    Adición Sección Filtros y KPIs

--- src/app_tablero.py
+++ src/app_tablero.py
@@ -176,11 +176,343 @@ def generate_control_card():
     )
 
 
+def generate_filters():
+    """
+    :return: A Div dropdown lists for filters.
+    """
+    return html.Div(
+        id="filters",
+        children=[
+            # Canal
+            html.H2("Filtros",style={
+                        "textAlign": "Left",
+                        "marginBottom": "20px",
+                        "color": "#FFFFFF",  # Puedes cambiar el color si lo deseas
+                    }),
+            html.Div(
+                id="componentes-fecha-inicial",
+                children=[
+                    
+                    html.Div(
+                        id="componente-anio",
+                        children=[
+                            html.P("Año"),
+                            dcc.Dropdown(
+                                id="anio-dropdown",
+                                options=[{'label': anio, 'value': anio} for anio in data_ad['Año'].unique()],
+                                placeholder="Seleccione un Año",
+                                value=data_ad['Año'].unique()[0],
+                                multi = True,
+                                style=dict(width='50%', minWidth='300px')
+                            )
+                        ],
+                        style=dict(width='20%')
+                    ),
+                    html.Div(
+                        id="componente-mes",
+                        children=[
+                            html.P("Mes"),
+                            dcc.Dropdown(
+                                id="mes-dropdown",
+                                options=[{'label': mes, 'value': mes} for mes in data_ad['Mes'].unique()],
+                                placeholder="Seleccione uno o varios Mes",
+                                value=data_ad['Mes'].unique()[0],
+                                multi = True,
+                                style=dict(width='50%', minWidth='300px')
+                            )
+                        ],
+                        style=dict(width='20%')
+                    ),
+                    html.Div(
+                        id="componente-uen",
+                        children=[
+                            html.P("UEN"),
+                            dcc.Dropdown(
+                                id="uen-dropdown",
+                                options=[{'label': uen, 'value': uen} for uen in data_ad['Uen'].unique()],
+                                placeholder="Seleccione una Unidad de Negocio",
+                                value=data_ad['Uen'].unique()[0],
+                                multi = True,
+                                style=dict(width='50%', minWidth='300px')
+                            )
+                        ],
+                        style=dict(width='20%')
+                    ),
+
+                    html.Div(
+                        id="componente-canal",
+                        children=[
+                            html.P("Canal"),
+                            dcc.Dropdown(
+                                id="canal-dropdown",
+                                options=[{'label': canal, 'value': canal} for canal in data_ad['Canal Comercial'].unique()],
+                                placeholder="Seleccione un canal",
+                                value=data_ad['Canal Comercial'].unique()[0],
+                                multi = True,
+                                style=dict(width='50%', minWidth='300px')
+                            )
+                        ],
+                        style=dict(width='20%')
+                    ),
+
+                    html.Div(
+                        id="componente-regional",
+                        children=[
+                            html.P("Regional"),
+                            dcc.Dropdown(
+                                id="regional-dropdown",
+                                options=[{'label': regional, 'value': regional} for regional in data_ad['Regional'].unique()],
+                                placeholder="Seleccione una Regional",
+                                value=data_ad['Regional'].unique()[0],
+                                multi = True,
+                                style=dict(width='50%', minWidth='300px')
+                            )
+                        ],
+                        style=dict(width='20%')
+                    ),
+                    html.Div(
+                        id="componente-marquilla",
+                        children=[
+                            html.P("Marquilla"),
+                            dcc.Dropdown(
+                                id="marquilla-dropdown",
+                                options=[{'label': marquilla, 'value': marquilla} for marquilla in data_ad['Marquilla'].unique()],
+                                placeholder="Seleccione una Marquilla",
+                                value=data_ad['Marquilla'].unique()[0],
+                                multi = True,
+                                style=dict(width='50%', minWidth='300px')
+                            )
+                        ],
+                        style=dict(width='20%')
+                    ),
+                    html.Div(
+                        id="componente-producto",
+                        children=[
+                            html.P("Producto"),
+                            dcc.Dropdown(
+                                id="producto-dropdown",
+                                options=[{'label': producto, 'value': producto} for producto in data_ad['Producto'].unique()],
+                                placeholder="Seleccione un Producto",
+                                value=data_ad['Producto'].unique()[0],
+                                multi = True,
+                                style=dict(width='50%', minWidth='300px')
+                            )
+                        ],
+                        style=dict(width='20%')
+                    ),
+                ],
+                style=dict(display='flex',flexDirection='column',gap='10px',marginBottom = '20px') # Organiza los filtros en columna
+            ),
+        ]
+    )
+
+
+def generate_KPI():
+    """
+    Función para generar 4 KPIs, cada uno en su propia tarjeta.
+    :return: Una lista de Divs que representan los 4 KPIs.
+    """
+    return html.Div(
+        className="row",  # Utilizamos un 'row' para alinear los KPIs horizontalmente
+        children=[
+            # KPI 1
+            html.Div(
+                className="two columns",  # Cada KPI ocupa 3 columnas de 12 (1/4 del espacio total)
+                style={"flex":"1","width":"25%"},
+                children=[
+                    html.Div(
+                        className="kpi-card",  # Clase para estilizar cada tarjeta de KPI
+                        children=[
+                            html.H4("Total Ventas COP", style={"textAlign": "center", "color": "#FFFFFF"}),
+                            html.P("1",  # Un valor aleatorio para ejemplo
+                                   style={"textAlign": "center", "fontSize": "30px", "color": "#FFFFFF"}),
+                        ],
+                        style={
+                            "padding": "20px",
+                            "borderRadius": "8px",
+                            "color": "#FFFFFF",
+                            "textAlign": "center",
+                        },
+                    )
+                ],
+            ),
+            # KPI 2
+            html.Div(
+                className="two columns",
+                style={"flex":"1","width":"25%"},
+                children=[
+                    html.Div(
+                        className="kpi-card",
+                        children=[
+                            html.H4("Total Ventas gl", style={"textAlign": "center", "color": "#FFFFFF"}),
+                            html.P("2",  # Un valor aleatorio
+                                   style={"textAlign": "center", "fontSize": "30px", "color": "#FFFFFF"}),
+                        ],
+                        style={
+                            "padding": "20px",
+                            "borderRadius": "8px",
+                            "color": "#FFFFFF",
+                            "textAlign": "center",
+                        },
+                    )
+                ],
+            ),
+            # KPI 3
+            html.Div(
+                className="two columns",
+                style={"flex":"1","width":"30%"},
+                children=[
+                    html.Div(
+                        className="kpi-card",
+                        children=[
+                            html.H4("Utilidad Bruta COP", style={"textAlign": "center", "color": "#FFFFFF"}),
+                            html.P("3",  # Un valor aleatorio
+                                   style={"textAlign": "center", "fontSize": "30px", "color": "#FFFFFF"}),
+                        ],
+                        style={
+                            "padding": "20px",
+                            "borderRadius": "8px",
+                            "color": "#FFFFFF",
+                            "textAlign": "center",
+                        },
+                    )
+                ],
+            ),
+            # KPI 4
+            html.Div(
+                className="two columns",
+                style={"flex":"1","width":"10%"},
+                children=[
+                    html.Div(
+                        className="kpi-card",
+                        children=[
+                            html.H4("Margen", style={"textAlign": "center", "color": "#FFFFFF"}),
+                            html.P("4",  # Un valor aleatorio
+                                   style={"textAlign": "center", "fontSize": "30px", "color": "#FFFFFF"}),
+                        ],
+                        style={
+                            "padding": "10px",
+                            "borderRadius": "8px",
+                            "color": "#FFFFFF",
+                            "textAlign": "center",
+                        },
+                    )
+                ],
+            ),
+        ]
+    )
+
+# def plot_time_series_1(data_ad):
+
+#     serie_ventas = data_ad.groupby("Marquilla")["Ventas"].sum().reset_index()
+#     ventas_por_marquilla = ventas_por_marquilla.sort_values(by="Ventas", ascending=False)
+#     ventas_por_marquilla = ventas_por_marquilla.head(10)
+
+#     fig = go.Figure(
+#         data=[
+#             go.Bar(
+#                 x=ventas_por_marquilla["Marquilla"],
+#                 y=ventas_por_marquilla["Ventas"],
+#                 marker=dict(color="#3498db"),
+#                 name="Ventas por Marquilla"
+#             )
+#         ]
+#     )
+
+#     # Configuración del diseño del gráfico
+#     fig.update_layout(
+#         title="Total de Ventas por Marquilla",
+#         xaxis_title="Marquilla",
+#         yaxis_title="Ventas",
+#         paper_bgcolor="rgba(0,0,0,0)",  # Fondo transparente (usa color hexadecimal si prefieres)
+#         plot_bgcolor="#E8E8E8",  # Fondo del área de la gráfica
+#         font=dict(color="#FFFFFF")  # Color de texto de los ejes y título
+#     )
+
+#     return fig
+
+# def plot_time_series_2(data_ad):
+
+#     ventas_por_marquilla = data_ad.groupby("Marquilla")["Ventas"].sum().reset_index()
+#     ventas_por_marquilla = ventas_por_marquilla.sort_values(by="Ventas", ascending=False)
+#     ventas_por_marquilla = ventas_por_marquilla.head(10)
+
+#     fig = go.Figure(
+#         data=[
+#             go.Bar(
+#                 x=ventas_por_marquilla["Marquilla"],
+#                 y=ventas_por_marquilla["Ventas"],
+#                 marker=dict(color="#3498db"),
+#                 name="Ventas por Marquilla"
+#             )
+#         ]
+#     )
+
+#     # Configuración del diseño del gráfico
+#     fig.update_layout(
+#         title="Total de Ventas por Marquilla",
+#         xaxis_title="Marquilla",
+#         yaxis_title="Ventas",
+#         paper_bgcolor="rgba(0,0,0,0)",  # Fondo transparente (usa color hexadecimal si prefieres)
+#         plot_bgcolor="#E8E8E8",  # Fondo del área de la gráfica
+#         font=dict(color="#FFFFFF")  # Color de texto de los ejes y título
+#     )
+
+#     return fig
+
+
 app.layout = html.Div(
     id="app-container",
 
     children=[
         dcc.Interval(id="interval", interval=1000, n_intervals=0),
+
+        # Sección 1 - Filtros
+        html.Div(
+            id="section-1",
+            # style={
+            #     "backgroundColor": "#E10110",  # Cambia este valor al color que prefieras
+            #     "color": "#FFFFFF",  # Color del texto en la página para que contraste con el fondo
+            #     "padding": "10px",   # Espaciado interno opcional
+            # },
+            children=[
+                # Título de la sección de gráficos
+                html.H2(
+                    "KPIs",  # Cambia este texto al título que desees
+                    style={
+                        "textAlign": "center",
+                        "marginBottom": "20px",
+                        "color": "#FFFFFF",  # Puedes cambiar el color si lo deseas
+                    }
+                ),
+                
+                # First row with two side-by-side graphs
+                html.Div(
+                    className="row",
+                    children=[
+                        # First graph on the left
+                        html.Div(
+                            className="three columns",
+                            style={"display": "flex", "justify-content": "center", "align-items": "center", 'height': '70vh'},
+                            children=[generate_filters()]
+                            + [
+                                html.Div(
+                                    ["initial child"], id="output-clientside", style={"display": "none"}
+                                )
+                            ],
+                        ),
+                        # Second graph on the right
+                        html.Div(
+                            className="six columns",    
+                            style={"display": "flex"},
+                            children=[
+                                generate_KPI()
+                            ],
+                        ),
+                    ],
+                ),
+            ],
+        ),
 
         # Sección 2 - Análisis descriptivo de venta
         html.Div(
@@ -303,7 +635,14 @@ app.layout = html.Div(
      Output(component_id="plot_series_3", component_property="figure"),
      Output(component_id="plot_series_5", component_property="figure")],
     [Input("interval", "n_intervals"),
-     Input(component_id="canal-dropdown", component_property="value")]
+     Input(component_id="canal-dropdown", component_property="value"),
+    #  Input(component_id="anio-dropdown", component_property="value"),
+    #  Input(component_id="mes-dropdown", component_property="value"),
+    #  Input(component_id="marquilla-dropdown", component_property="value"),
+    #  Input(component_id="regional-dropdown", component_property="value"),
+    #  Input(component_id="uen-dropdown", component_property="value"),
+    #  Input(component_id="producto-dropdown", component_property="value")>
+     ]
 )
 def update_output_div(n_intervals, canal):
 
