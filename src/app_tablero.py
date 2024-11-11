b9e5274be271e6f594749a904c1f15bb9d64d330
Luis David <luisdavidg.98@gmail.com>
2024-11-10 23:18:45 -0500

    Nueva versión tablero con inclusión de esquema de la sección predictiva

--- src/app_tablero.py
+++ src/app_tablero.py
@@ -38,67 +38,6 @@ def load_data_analisis_descriptivo():
 #data = load_data()
 data_ad = load_data_analisis_descriptivo()
 
-# Graficar serie
-def plot_series(data, initial_date, proy):
-    data_plot = data.loc[initial_date:]
-    data_plot = data_plot[:-(120-proy)]
-    fig = go.Figure([
-        go.Scatter(
-            name='Demanda energética',
-            x=data_plot.index,
-            y=data_plot['AT_load_actual_entsoe_transparency'],
-            mode='lines',
-            line=dict(color="#188463"),
-        ),
-        go.Scatter(
-            name='Proyección',
-            x=data_plot.index,
-            y=data_plot['forecast'],
-            mode='lines',
-            line=dict(color="#bbffeb",),
-        ),
-        go.Scatter(
-            name='Upper Bound',
-            x=data_plot.index,
-            y=data_plot['Upper bound'],
-            mode='lines',
-            marker=dict(color="#444"),
-            line=dict(width=0),
-            showlegend=False
-        ),
-        go.Scatter(
-            name='Lower Bound',
-            x=data_plot.index,
-            y=data_plot['Lower bound'],
-            marker=dict(color="#444"),
-            line=dict(width=0),
-            mode='lines',
-            fillcolor="rgba(242, 255, 251, 0.3)",
-            fill='tonexty',
-            showlegend=False
-        )
-    ])
-
-    fig.update_layout(
-        legend=dict(
-            orientation="h",
-            yanchor="bottom",
-            y=1.02,
-            xanchor="right",
-            x=1
-        ),
-        yaxis_title='Demanda total [MW]',
-        #title='Continuous, variable value error bars',
-        hovermode="x"
-    )
-    #fig = px.line(data2, x='local_timestamp', y="Demanda total [MW]", markers=True, labels={"local_timestamp": "Fecha"})
-    fig.update_layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', font_color="#2cfec1")
-    fig.update_xaxes(showgrid=True, gridwidth=0.25, gridcolor='#7C7C7C')
-    fig.update_yaxes(showgrid=True, gridwidth=0.25, gridcolor='#7C7C7C')
-    #fig.update_traces(line_color='#2cfec1')
-
-    return fig
-
 def plot_bar_1(data_ad):
 
     ventas_por_canal = data_ad.groupby("Canal Comercial")["Ventas"].sum().reset_index()
@@ -211,37 +150,21 @@ def generate_control_card():
     return html.Div(
         id="control-card",
         children=[
-
-            # Fecha inicial
-            html.P("Seleccionar fecha y hora inicial:"),
-
+            # Canal
+            html.P("Seleccionar un Canal Comercial:"),
             html.Div(
                 id="componentes-fecha-inicial",
                 children=[
-                    html.Div(
-                        id="componente-fecha",
-                        children=[
-                            dcc.DatePickerSingle(
-                                id='datepicker-inicial',
-                                min_date_allowed=min(data.index.date),
-                                max_date_allowed=max(data.index.date),
-                                initial_visible_month=min(data.index.date),
-                                date=max(data.index.date)-dt.timedelta(days=7)
-                            )
-                        ],
-                        style=dict(width='30%')
-                    ),
-                    
-                    html.P(" ",style=dict(width='5%', textAlign='center')),
                     
                     html.Div(
-                        id="componente-hora",
+                        id="componente-canal",
                         children=[
                             dcc.Dropdown(
-                                id="dropdown-hora-inicial-hora",
-                                options=[{"label": i, "value": i} for i in np.arange(0,25)],
-                                value=pd.to_datetime(max(data.index)-dt.timedelta(days=7)).hour,
-                                # style=dict(width='50%', display="inline-block")
+                                id="canal-dropdown",
+                                options=[{'label': canal, 'value': canal} for canal in data_ad['Canal Comercial'].unique()],
+                                placeholder="Seleccione un canal",
+                                value=data_ad['Canal Comercial'].unique()[0],
+                                style=dict(width='50%', minWidth='300px')
                             )
                         ],
                         style=dict(width='20%')
@@ -249,26 +172,6 @@ def generate_control_card():
                 ],
                 style=dict(display='flex')
             ),
-
-            html.Br(),
-
-            # Slider proyección
-            html.Div(
-                id="campo-slider",
-                children=[
-                    html.P("Ingrese horas a proyectar:"),
-                    dcc.Slider(
-                        id="slider-proyeccion",
-                        min=0,
-                        max=119,
-                        step=1,
-                        value=0,
-                        marks=None,
-                        tooltip={"placement": "bottom", "always_visible": True},
-                    )
-                ]
-            )     
-     
         ]
     )
 
@@ -278,9 +181,10 @@ app.layout = html.Div(
 
     children=[
         dcc.Interval(id="interval", interval=1000, n_intervals=0),
-        # Right column (now the only column with three graphs)
+
+        # Sección 2 - Análisis descriptivo de venta
         html.Div(
-            id="right-column",
+            id="section-2",
             className="twelve columns",
             # style={
             #     "backgroundColor": "#E10110",  # Cambia este valor al color que prefieras
@@ -340,6 +244,54 @@ app.layout = html.Div(
                 ),
             ],
         ),
+
+        # Sección 3 - Gráfica de análisis predictivo
+        html.Div(
+            id="section-3",
+            # style={
+            #     "backgroundColor": "#E10110",  # Cambia este valor al color que prefieras
+            #     "color": "#FFFFFF",  # Color del texto en la página para que contraste con el fondo
+            #     "padding": "10px",   # Espaciado interno opcional
+            # },
+            children=[
+                # Título de la sección de gráficos
+                html.H2(
+                    "Análisis predictivo",  # Cambia este texto al título que desees
+                    style={
+                        "textAlign": "center",
+                        "marginBottom": "20px",
+                        "color": "#FFFFFF",  # Puedes cambiar el color si lo deseas
+                    }
+                ),
+                
+                # First row with two side-by-side graphs
+                html.Div(
+                    className="row",
+                    children=[
+                        # First graph on the left
+                        html.Div(
+                            className="four columns",
+                            style={"display": "flex", "justify-content": "center", "align-items": "center", 'height': '70vh'},
+                            children=[generate_control_card()]
+                            + [
+                                html.Div(
+                                    ["initial child"], id="output-clientside", style={"display": "none"}
+                                )
+                            ],
+                        ),
+                        # Second graph on the right
+                        html.Div(
+                            className="eight columns",
+                            children=[
+                                html.B("Pronóstico de ventas por Canal"),
+                                html.Hr(),
+                                dcc.Graph(id="plot_series_5"),
+                            ],
+                        ),
+                    ],
+                ),
+            ],
+        ),
     ],
 )
 
@@ -348,16 +300,18 @@ app.layout = html.Div(
 @app.callback(
     [Output(component_id="plot_series_1", component_property="figure"),
      Output(component_id="plot_series_2", component_property="figure"),
-     Output(component_id="plot_series_3", component_property="figure")],
-    [Input("interval", "n_intervals")]
+     Output(component_id="plot_series_3", component_property="figure"),
+     Output(component_id="plot_series_5", component_property="figure")],
+    [Input("interval", "n_intervals"),
+     Input(component_id="canal-dropdown", component_property="value")]
 )
-def update_output_div(n_intervals):
+def update_output_div(n_intervals, canal):
 
     fig1 = plot_bar_1(data_ad)   
     fig2 = plot_bar_2(data_ad)  
     fig3 = plot_pie_chart(data_ad)
 
-    return fig1, fig2, fig3
+    return fig1, fig2, fig3, go.Figure()
 
 
 # Run the server
